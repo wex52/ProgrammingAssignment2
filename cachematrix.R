6de222ef8bda2ea533b24f86489ba76597f2d79a
wex52 <wechsler@bellsouth.net>
2017-08-27 19:22:52 -0400

    Version 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,32 @@
-## Check GitHub
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The makeCacheMatrix function builds a set of four functions and
+## returns the functions within a list. Each function has its own environment.
+## The function will also have two data objects, x and s.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { # initialize x, avoid call error
+    s <- NULL                               # initialize s
+    set <- function(y) { # mutator method
+        x <<- y          # assign input y to x object in makeCacheMatrix parent
+        s <<- NULL       # clears previous s, if x is reset
+    }
+    get <- function() x  # retrieves x from makeCacheMatrix parent
+    setsolve <- function(solve) s <<- solve  # sets value of s in parent
+    getsolve <- function() s                 # gets value of s from parent
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## The cacheSolve function returns a matrix that is the inverse of x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()                    # attempts to retrieve solution of x
+    if(!is.null(s)) {                    # if s already exists, returns it
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()        # s is NULL. Gets matrix.
+    s <- solve(data, ...)  # solves matrix
+    x$setsolve(s)          # sets solution
+    s                      # returns solution
 }
